00197bda8c7883c0c70babe3f35ad5b1e52e46a5
Gerard <gerardzon@yahoo.com>
2014-08-26 13:08:28 -0400

    another stuff

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -81,7 +81,7 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display 
-	  config.add_index_field 'thumbnail_display', :label => 'Thumbnail'
+	  config.add_index_field 'Image', :label => 'Thumbnail'
     config.add_index_field 'title_display', :label => 'Title'
     config.add_index_field 'title_vern_display', :label => 'Title'
     config.add_index_field 'author_display', :label => 'Author'
@@ -94,7 +94,6 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-	  config.add_show_field 'thumbnail_display', :label => 'Thumbnail'
     config.add_show_field 'title_display', :label => 'Title'
     config.add_show_field 'title_vern_display', :label => 'Title'
     config.add_show_field 'subtitle_display', :label => 'Subtitle'
