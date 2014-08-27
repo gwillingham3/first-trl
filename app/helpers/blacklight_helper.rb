da3a6ff9adeb3c769a7a30f58ed408ce04e8928b
Gerard <gerardzon@yahoo.com>
2014-08-27 16:39:32 -0400

    another stuff

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -1,3 +1,19 @@
 module BlacklightHelper
   include Blacklight::BlacklightHelperBehavior
+  
+  def application_name
+	"TRL"
+  end
+  
+  ##
+  # A list of document partial templates to attempt to render
+  #
+  # @see #render_document_index_with_view
+  # @return [Array<String>]
+  def document_index_path_templates
+    # first, the legacy template names for backwards compatbility
+    # followed by the new, inheritable style
+    # finally, a controller-specific path for non-catalog subclasses
+    @document_index_path_templates ||= ["catalog/document_gallery"]
+  end
 end
