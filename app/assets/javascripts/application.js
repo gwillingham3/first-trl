3d7686b1f2e3c33ca1fbf6b06f4423abf79b2a18
Gerard <gerardzon@yahoo.com>
2014-09-01 23:32:19 -0400

    another stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap
-//= require bootstrap/tooltip.js
-//= require bootstrap/popover.js
+//= require bootstrap.min
 //
 // Required by Blacklight
 //= require blacklight/blacklight
